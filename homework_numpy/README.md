ef6177f3eb51b253590d2c927262d3190108e499
NatashaKhotkina <natashakhotkina@gmail.com>
2022-10-27 22:04:59 +0400

    Homework3 finally

---
+++ homework_numpy/README.md
@@ -0,0 +1,18 @@
+# Numpy functions
+
+To run this script install requirements:
+
+`pip install -r requirements.txt`
+
+To import this script run:
+
+`import numpy_challenge`
+
+After import following functions are available:
+
+1) matrix_multiplication(mat1, mat2) - multiplies two matrices
+2) multiplication_check(list of matrices) - checks whether matrices can be multiplied in following order
+3) multiply_matrices(list of matrices) - if possible multiplies in following order
+4) compute_2d_distance(arr1, arr2) - computes Euclidian distance
+5) compute_multidimensional_distance(arr1, arr2) -  computes Euclidian distance
+6) compute_pair_distances(array) -  computes Euclidian distance between lines
---
+++ homework_numpy/numpy_challenge.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+
+def matrix_multiplication(x1, x2):
+    return np.matmul(x1, x2)
+
+
+def multiplication_check(matrices):
+    flag = True
+    for idx in range(len(matrices) - 1):
+        if matrices[idx].shape[1] != matrices[idx + 1].shape[0]:
+            flag = False
+    return flag
+
+
+def multiply_matrices(matrices):
+    if not multiplication_check(matrices):
+        return None
+    else:
+        multiplier = matrices[0]
+        for matrix in matrices[1:]:
+            multiplier = np.matmul(multiplier, matrix)
+        return multiplier
+
+
+def compute_2d_distance(arr1, arr2):
+    return np.linalg.norm(arr1 - arr2)
+
+
+def compute_multidimensional_distance(arr1, arr2):
+    return np.linalg.norm(arr1 - arr2)
+
+
+def compute_pair_distances(arr):
+    n = arr.shape[0]
+    answer = np.zeros((n, n))
+    for i in range(n):
+        for j in range(n):
+            answer[i, j] = np.linalg.norm(arr[i] - arr[j])
+    return answer
+
+
+if __name__ == "__main__":
+    first_array = np.full((3, 3), 1)
+    second_array = np.eye(3)
+    third_array = np.arange(0, 20, 2).reshape(2, 5)
