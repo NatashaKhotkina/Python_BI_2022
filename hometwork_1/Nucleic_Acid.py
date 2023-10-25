741417f68d22f804349bb1553c4effc98f063f81
NatashaKhotkina <natashakhotkina@gmail.com>
2023-10-26 00:06:27 +0400

    Simplified input check and transcribe command.

--- hometwork_1/Nucleic_Acid.py
+++ hometwork_1/Nucleic_Acid.py
@@ -1,6 +1,7 @@
-good_letters = ['a', 't', 'g', 'c', 'u']
+good_letters = {'a', 't', 'g', 'c', 'u'}
 complement_dict_DNA = {'a': 't', 'A': 'T', 't': 'a', 'T': 'A', 'g': 'c', 'G': 'C', 'c': 'g', 'C': 'G'}
 complement_dict_RNA = {'g': 'c', 'G': 'C', 'c': 'g', 'C': 'G', 'a': 'u', 'A': 'U', 'u': 'a', 'U': 'A'}
+transcribe_dict = {'t': 'u', 'T': 'U', 'a': 'a', 'A': 'A', 'g': 'g', 'G': 'G', 'c': 'c', 'C': 'C'}
 known_commands = ['exit', 'transcribe', 'reverse', 'complement', 'reverse complement']
 while True:
     new_sequence = ''
@@ -14,34 +15,22 @@ while True:
         continue
 
 # If the command is in the list and is not 'exit' - we should read the sequence first.
-    condition = 'no such acid'
-    while condition == 'no such acid':
+    while True:
         nucl_acid = input('Type the sequence:')
-        condition = 'stop'
-        for i in nucl_acid.lower():
-            if i not in good_letters:
-                condition = 'no such acid'
-        if 'u' in nucl_acid.lower() and 't' in nucl_acid.lower():
-            condition = 'no such acid'
-        if condition == 'no such acid':
+        if not set(nucl_acid.lower()) <= good_letters or {'u', 't'} <= set(nucl_acid.lower()):
             print('No such nucleic acid. Try again!')
+        else:
+            break
 
     if command == 'transcribe':
         if 'u' in nucl_acid.lower():
             print('Cannot transcribe RNA. Try again!')
-        else:
-            for i in nucl_acid:
-                if i == 't':
-                    new_sequence += 'u'
-                elif i == 'T':
-                    new_sequence += 'U'
-                else:
-                    new_sequence += i
-        print(new_sequence)
+            continue
+        new_sequence = ''.join([transcribe_dict[nucl] for nucl in nucl_acid])
+
 
     elif command == 'reverse':
         new_sequence = nucl_acid[::-1]
-        print(new_sequence)
 
     elif command == 'complement':
         if 'u' in nucl_acid.lower():
@@ -50,7 +39,6 @@ while True:
         else:
             for i in nucl_acid:
                 new_sequence += complement_dict_DNA[i]
-        print(new_sequence)
 
     elif command == 'reverse complement':
         if 'u' in nucl_acid.lower():
@@ -60,7 +48,8 @@ while True:
             for i in nucl_acid:
                 new_sequence += complement_dict_DNA[i]
         new_sequence = new_sequence[::-1]
-        print(new_sequence)
+
+    print(new_sequence)
 
 
 
